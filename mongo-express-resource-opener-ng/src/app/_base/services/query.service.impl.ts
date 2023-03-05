b96f55d784630e9f101297359a899547e5c98d47
Matej Kováč <matej.kovac@powerflow.cz>
2023-03-05 21:54:20 +0100

    set up datasets urls loading for requests firing

--- mongo-express-resource-opener-ng/src/app/_base/services/query.service.impl.ts
+++ mongo-express-resource-opener-ng/src/app/_base/services/query.service.impl.ts
@@ -1,8 +1,9 @@
-import { EnviromentUtil } from 'src/app/_base/utils/enviroment.util';
-import { StoreService } from './store.service';
-import { SettingsNames } from './../utils/enviroment.util';
-import { QueryService } from './query.service';
 import { Injectable } from "@angular/core";
+import { EnviromentUtil, SettingsNames } from 'src/app/_base/utils/enviroment.util';
+import { DataSetsStoreRecordFormat } from './../components/_base/data-sets/data-sets.interfaces';
+import { KeyValuePair, QueryService, Settings } from './query.service';
+import { StoreService } from './store.service';
+
 
 @Injectable({
     providedIn : 'root'
@@ -28,57 +29,78 @@ export class QueryServiceImpl implements QueryService {
         });
     }
 
+    /** HELPING METHODS ------------------------------------------------------------------------------------------- */
 
     private check(resourceId : string) : Promise<any> {
         var context = this;
-        // return new Promise((resolve, reject) => {
-
-        // });
         return Promise.all([
             context.loadAllSettings()
         ])
-        .then((settings : any) => {
-            //console.log('all settings resolved promises loaded');
-            //console.log(result);
-            
+        .then((settings : Settings) => {
+            context.fireRequests(resourceId, settings)
         });
     }
 
-    private fireRequests() : Promise<any> {
+    private fireRequests(resourceId : string, settings : Settings) : Promise<any> {
+        var context = this;
         return new Promise((resolve, reject) => { 
-            
+
+            let requestsPromises : Promise<any>[] = context
+                .getDatasourcesUrls(settings, resourceId)
+                .map((url) => fetch(url))
+
+            Promise.allSettled(requestsPromises)
+                .then((responses) => {
+                    console.log('RESPONSE');
+                    console.log(responses);
+                    //TODO continue 
+                    // https://github.com/K0V0/mongoExpressResourceOpener/blob/51896f6e3b8b78b1a9234c58790a93c92f22d74f/js/background_helper.js#L109
+                })
         });
     }
 
-    private loadAllSettings() : Promise<any> {
+    private getDatasourcesUrls(settings : Settings, resourceId : string) : string[] {
+
+        let searchEverywhere : boolean = this.extractSetting(settings, SettingsNames.CHECK_ON_ALL_ENVIROMENTS);
+        let currentEnviroment : number = this.extractSetting(settings, SettingsNames.CURRENT_ENVIROMENT);
+
+        return this.extractSetting(settings, SettingsNames.ENVIROMENTS)
+                .filter((enviroment : DataSetsStoreRecordFormat) => searchEverywhere ? true : enviroment.id === currentEnviroment)
+                .flatMap((enviroment : DataSetsStoreRecordFormat) => enviroment.datasets)
+                .map((datasourceUrl : string) => (datasourceUrl.lastIndexOf("/") === datasourceUrl.length) ? datasourceUrl : datasourceUrl + "/")
+                .map((datasourceUrl :string) => datasourceUrl + "\"" + resourceId + "\"");
+    }
+
+    private loadAllSettings() : Promise<Settings> {
         return new Promise((resolve, reject) => {
 
-            let settingsPromises : Promise<any>[] = [];
+            let settingsPromises : Promise<KeyValuePair>[] = [];
             Object.values(SettingsNames).forEach((settingName : string) => {
                 settingsPromises.push(this.storeService.loadWithKey(settingName));
             })
 
-            Promise.allSettled(settingsPromises).then((results) => {
-                // console.log('all settings promises settled');
-                //console.log(results);
-                // let settings : any = results
-                //     .filter((result) => result.status === 'fulfilled')
-                //     .map((result) => result.value);
-                let settings : any[] = []; 
-                results.forEach((result) => {
-                    // console.log(result.status);
-                    if (result.status === 'fulfilled') {
-                        // console.log(result.value);
-                        settings.push(result.value);
-                    }
-                });
-
-                resolve(settings);
-            }).catch(() => {
-                reject('Error during loading settings from store - promises settling');
-            })
-
+            Promise.allSettled(settingsPromises)
+                .then((results) => {
+                    let settings : Settings = []; 
+                    results.forEach((result) => {
+                        //TODO really need to be that shitty ? ask stackoverflow why cannot have prettier one liner
+                        if (result.status === 'fulfilled') {
+                            settings.push(result.value);
+                        }
+                    });
+                    resolve(settings);
+                })
+                .catch(() => {
+                    reject('Error during loading settings from store - promises settling');
+                })
         });
+    }
+
+    private extractSetting(settings : Settings, settingType : SettingsNames) : any {
+        return settings
+            .flatMap((setting) => setting)
+            .filter((setting : KeyValuePair) => Object.keys(setting)[0] === settingType)
+            .find((x) => x)[settingType]
     }
 
 }
